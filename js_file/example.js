4d818b960e3f5f4feb72bb527deda66b57707ef8
Marnie Cannon <marnie25@gmail.com>
2015-11-02 19:47:56 -0800

    initial commit

---
+++ js_file/example.js
@@ -0,0 +1,17 @@
+console.log('where am I?');
+var someNumber = 5;
+var anotherNumber = someNumber * 8;
+console.log(someNumber);
+console.log(anotherNumber);	
+var name='Marnie';
+var aLocation = 'University of Washington';
+var quote = 'a greate man once said \'always do stuff\'';
+console.log(quote);
+var favFoods = ['pizza', 'ice cream', 'chocolate'];
+console.log(favFoods);
+console.log(favFoods[0]);
+console.log(favFoods[2]);
+
+var thirdFav = favFoods[2];
+console.log(thirdFav);
+
